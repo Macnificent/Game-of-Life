abb38e8219c970a6b8f4994404157be3fc22d6b1
Macnificent <marcus.folkeryd@gmail.com>
2013-02-08 20:55:34 +0100

    Forgot shit

--- js/life.js
+++ js/life.js
@@ -4,19 +4,21 @@
 */
 
 
-var scene, camera;
+var scene, camera, controls;
 var renderer, projector;
+var clock = new THREE.Clock();
 
 var _PLAYBACKSTATE;
-var _zoomValue = 200;
+//var _zoomValue = 400;
 var _cellGrid;
 var _cellPosGrid = [];
-var cellAliveColor = 0x000000;
-var cellDeadColor = 0xCCCCCC;//0xDADADA;
+var cellAliveColor = 0x000000;//0xCF7902;//
+var cellDeadColor = 0xCCCCCC;//0x696969; //
 var _latestUpdate;
 var cellUpdateArray = [];
-var _shiftKeyState = false;
+var _cameraLock = false;
 var _tickDelay = 100; //Default
+var requestedSizeAnimations = [];
 
 var screenW = window.innerWidth;
 var screenH = window.innerHeight; 
@@ -26,33 +28,50 @@ var filterStrength = 20;
 var frameTime = 0, lastLoop = new Date, thisLoop;
 
 var keyDownMappings = {	
-	 32: setPlayBackState, //Space
-	 16: function(){ //Shift
-	 	setShiftKeyState(true);
+	 80: setPlayBackState, //P
+	 81: function(){ //Q
+	 	setCameraLock();
 	 },
 	 70: doFullScreen, //F key
 	 82: function(){
 	 	resetLife();	
-	 },
+	 }, //R key
 	 67: function(){
 	 	showHideControls();	
-	 } //R key
+	 } //C key
 };
 
 var keyUpMappings = { 
-	 16: function(){ //Shift
-	 	setShiftKeyState(false);
-	 }	
 };
 
-function setShiftKeyState(boolState){
-	if(_shiftKeyState != boolState){
-		_shiftKeyState = boolState;
-	}
-	console.log("shift " + boolState);
+function setCameraLock(){
+	_cameraLock = !_cameraLock;
 }
 
 function initEventHandlers(){
+	$("#toolTip").qtip({
+	    content:{
+	      prerender : true,
+	      text:  tmpl("seedSelectorTmpl", {})
+	    },
+	    style: {
+	      width: 400,
+	      background: 'none',
+	      border: 'none'
+	    },
+	//Position should ideally be at mouse xy
+	position: {
+		my: 'top left',
+		viewport: $(document),
+		adjust: {
+			x: $(document).width()/2,
+			y: $(document).height()/2
+		}
+	}
+	});
+	
+	$(document)[0].oncontextmenu = function() {return false;}  
+
 	$(document).keydown(function(e) {
 		if(keyDownMappings[e.which]){
 			keyDownMappings[e.which]();	
@@ -82,16 +101,28 @@ function initEventHandlers(){
 	    }   
 	});	
 	
-	$('canvas').click(function(event){
-		findIntersectingCell(event.clientX, event.clientY, "click");
+	$('canvas').mousedown(function(event){
+		switch(event.which){
+			case 1:
+				findIntersectingCell(event.clientX, event.clientY, "click");		
+			break;
+			case 3:
+				//findIntersectingCell(event.clientX, event.clientY, "rightClick");
+				var qtip = $("#toolTip").qtip('api');
+				//qtip.updatePosition(event, false);
+				qtip.show();
+				console.log(qtip);
+			break;
+		}
+
 	});
 	
 	
 	$('canvas').mousemove(function(event){
-		mouseX = event.clientX;
+		/*mouseX = event.clientX;
     	mouseY = event.clientY;
     	
-    	findIntersectingCell(event.clientX, event.clientY, "mouseOver");
+    	findIntersectingCell(event.clientX, event.clientY, "mouseOver");*/
 	});
 	
 	$(window).resize(function(){
@@ -104,10 +135,15 @@ function initEventHandlers(){
 }
 
 function findIntersectingCell(mouseX, mouseY, eventAction){
+	console.log(mouseX + " " + mouseY);
 	var vector = new THREE.Vector3( ( mouseX / window.innerWidth ) * 2 - 1, - ( mouseY / window.innerHeight ) * 2 + 1, 0.5 );
+	
+	//var vector = controls.target.clone().subSelf( controls.object.position ).normalize();
 	projector.unprojectVector( vector, camera );
 
 	var raycaster = new THREE.Raycaster( camera.position, vector.subSelf( camera.position ).normalize() );
+	
+	//var raycaster = new THREE.Ray( controls.position, vector.subSelf( camera.position ).normalize() );
 
 	var intersects = raycaster.intersectObjects(_cellGrid.children);
 
@@ -117,16 +153,19 @@ function findIntersectingCell(mouseX, mouseY, eventAction){
 				setCellState(intersects[0].object);
 			break;
 			case "mouseOver":
-				/*for(var i = 0; i < _cellGrid.children.length; i++){
+				/*for(var i = 0, len=_cellGrid.children.length; i<len;++i){
 					if(!_cellGrid.children[i]._cellState){
 						_cellGrid.children[i].material.opacity = 0.3;
 					}
-				}
+				}*/
 				
-				if(!intersects[0].object._cellState){
+				/*if(!intersects[0].object._cellState){
 					intersects[0].object.material.opacity = 0.7;
 				}*/
 	
+			break;
+			case "rightClick":
+				
 			break;
 		}
 	}		
@@ -152,18 +191,45 @@ function setPlayBackState(){
 	}
 	
 	changePlayBackIcon();
-	console.log(_PLAYBACKSTATE);
 }
 
 function initCanvas(){
-	
 	renderer = new THREE.WebGLRenderer();
-	camera = new THREE.PerspectiveCamera( 400, window.innerWidth / window.innerHeight, 1, 10000 );
-
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1100000);
 	scene = new THREE.Scene();
 	
-	renderer.setSize(window.innerWidth-20, window.innerHeight-20);
-	console.log(renderer.autoUpdateObjects);
+	var urlPrefix = "/Users/marcus/GoL_WebGL/style/img/skybox/";
+	var urls = [
+		urlPrefix + "Galaxy_BK.bmp",
+		urlPrefix + "Galaxy_FT.bmp",
+		urlPrefix + "Galaxy_UP.bmp",
+		urlPrefix + "Galaxy_DN.bmp",
+		urlPrefix + "Galaxy_RT.bmp",
+		urlPrefix + "Galaxy_LT.bmp"
+	];
+
+	var textureCube = THREE.ImageUtils.loadTextureCube( urls );
+	//textureCube.format = THREE.RGBFormat;
+	var shader = THREE.ShaderLib[ "cube" ];
+	shader.uniforms[ "tCube" ].value = textureCube;
+
+	cubematerial = new THREE.ShaderMaterial({
+	    fragmentShader: shader.fragmentShader,
+	    vertexShader: shader.vertexShader,
+	    uniforms: shader.uniforms,
+	    depthWrite: false,
+	    side: THREE.BackSide
+	});
+	skyBox = new THREE.Mesh(new THREE.CubeGeometry(10000,10000,10000), cubematerial);
+	scene.add(skyBox);
+
+	controls = new THREE.FirstPersonControls(camera);
+	controls.movementSpeed = 80;
+	controls.lookSpeed = 0.150;
+	controls.lookVertical = true;
+	camera.position.set( -300, 150, 0 );
+	
+	renderer.setSize(window.innerWidth-15, window.innerHeight-18);
 	$('#container').append(renderer.domElement);
 	
 	projector = new THREE.Projector();
@@ -171,49 +237,57 @@ function initCanvas(){
 	addCells();
 	_latestUpdate = +new Date();
 	tick();
-	
-	console.log("initCanvas");
 }
 
 function addCells(){
 	var cellWidth = 0.2;
 	var cellHeight = 0.2;
     var rowArray;
-    	  
+    var geo = new THREE.CubeGeometry(5, 5, 5);  
+    
     _cellGrid = new THREE.Object3D();
-
-     var gridDimensions = getGridDimensions();
+    var gridDimensions = getGridDimensions();
      
-     for (var j=0; j<gridDimensions.height; j++) {
-       rowArray = [];
-       for (var i=0; i<gridDimensions.width; i++) {
-         var mat = new THREE.MeshBasicMaterial({color: cellDeadColor, opacity: 0.3, transparent: true});	
-
-         var geo = new THREE.CubeGeometry(5, 5, 5);
-         var mesh = new THREE.Mesh(geo, mat);
-         mesh.autoUpdateMatrix = false;
-         
-         mesh.position.x = (i-gridDimensions.width/2) * 5.1;
-         mesh.position.y = 1;
-         mesh.position.z = -(j-gridDimensions.height/2) * 5.1;
-         mesh.castShadow = mesh.receiveShadow = true;
-		
-		//Cells are dead by default
-		mesh._cellState = false;
-		
-		if(typeof(_cellPosGrid[j]) != []){
-			_cellPosGrid[j] = [];
-		}
-		
-		rowArray.push(mesh.id);
-        _cellGrid.add(mesh);
+    for (var j=0; j<gridDimensions.height; j++) {
+        rowArray = [];
        
+    	for (var i=0; i<gridDimensions.width; i++) {
+    		
+			var mat = new THREE.MeshBasicMaterial({color: cellDeadColor, wireframe: false, linewidth: 10, opacity: 0.8, transparent: true});
+         	var mesh = new THREE.Mesh(geo, mat);
+         	mesh.autoUpdateMatrix = false;
+         	mesh.position.x = (i-gridDimensions.width/2) * 7.1;
+         	mesh.position.y = 1;
+         	mesh.position.z = -(j-gridDimensions.height/2) * 7.1;
+		
+			//Cells are dead by default
+			mesh._cellState = false;
+         		
+			if(typeof(_cellPosGrid[j]) != []){
+				_cellPosGrid[j] = [];
+			}
+			
+			rowArray.push(mesh);
+	        //scene.add(mesh);
+	        
+	        _cellGrid.add(mesh);
+
        }
        
        _cellPosGrid[j].push(rowArray);
      }
-	
+	//_cellGrid.autoUpdateMatrix = false;
 	scene.add(_cellGrid);
+}
+
+function posGridToArray(){
+	var cellArray = [];
+	for(var i = 0, rlen = _cellPosGrid.length; i < rlen; ++i){
+		for(var j = 0, clen = _cellPosGrid[i][0].length; j < clen; ++j){
+			cellArray.push(_cellPosGrid[i][0][j]);
+		}
+	}	
+	return cellArray;
 }
 
 function tick(){
@@ -230,15 +304,18 @@ function tick(){
 		}
 	}
 	
-	spdy =  (screenH / 2 - mouseY) / 100;
+	/*spdy =  (screenH / 2 - mouseY) / 100;
     spdx =  (screenW / 2 - mouseX) / 100;
     if (_shiftKeyState){
         _cellGrid.rotation.x = spdy;
         _cellGrid.rotation.y = spdx;
-    }
+    }*/
 	
-	camera.position.set( 100, _zoomValue, 50 );
-	camera.lookAt( scene.position );
+	//animateCubeSizes();
+	
+	/*camera.position.set( 100, _zoomValue, 50 );
+	camera.lookAt( scene.position );*/
+	controls.update( clock.getDelta() );
 	renderer.render(scene, camera);
 	
 	var thisFrameTime = (thisLoop=new Date) - lastLoop;
@@ -249,15 +326,29 @@ function tick(){
 function evaluateLife(){
 	var gridIndex;
 	
-	for(var i = 0; i < _cellGrid.children.length; i++){
-		current = _cellGrid.children[i];
-		
-		gridIndex = findCellIndex(current.id);
-		applyLifeRules(current, gridIndex);
-		
+	for(var i = 0, rlen= _cellPosGrid.length;i<rlen; ++i){
+		for(var j = 0, clen= _cellPosGrid[i][0].length;j<clen; ++j){
+			applyLifeRules(_cellPosGrid[i][0][j], [i, j]);
+		}
 	}
-
+	
 	applyLifeChanges();
+}
+
+function animateCubeSizes(){
+	var animationFinished = [];
+	for(var i=0, len=requestedSizeAnimations.length; i<len; ++i){
+		var currentCell = requestedSizeAnimations[i]._cellState;
+		if(currentCell._cellState){
+			if(current.scale.y <= 1.0){
+				requestedSizeAnimations[i] = null;
+			}else{
+					
+			}
+		}else{
+				
+		}
+	}
 }
 
 function getGridDimensions(){
@@ -270,25 +361,27 @@ function getGridDimensions(){
 function findCellIndex(cellID){
 	var columnIndex;
 	
-	for(var j = 0; j < _cellPosGrid.length; j++){
+	for(var j = 0,len=_cellPosGrid.length;j<len; ++j){
 		columnIndex = $.inArray(cellID, _cellPosGrid[j][0]);
 		
 		if(columnIndex != -1){
 			return [j, columnIndex]; 
 		}
 	}
-	
+
 	return false;
 }
 
 function setCellState(cell){
 	if(cell._cellState){
 		cell.material.color.setHex(cellDeadColor);
+		cell.material.wireframe = true;
 		cell.material.opacity = 0.3;
 		cell.scale.y = 1;
 		cell._cellState = false;
 	}else if(!cell._cellState){
 		cell.material.color.setHex(cellAliveColor);
+		cell.material.wireframe = false;
 		cell.material.opacity = 0.9;
 		cell.scale.y = 1.7;
 		cell._cellState = true;
@@ -304,7 +397,7 @@ function forceKillCell(cell){
 
 function applyLifeRules(cell, gridIndex){
 	var neighbourCount = getLivingNeighbours(gridIndex);
-	
+
 	if(cell._cellState){
 		if(neighbourCount != 2 && neighbourCount != 3){
 			if(neighbourCount < 2){ //Death by underpopulation
@@ -321,7 +414,7 @@ function applyLifeRules(cell, gridIndex){
 }
 
 function applyLifeChanges(){
-	for(var i = 0; i < cellUpdateArray.length; i++){
+	for(var i = 0, len=cellUpdateArray.length; i < len;++i){
 		setCellState(cellUpdateArray[i]);
 	}
 	cellUpdateArray.length = 0;
@@ -339,8 +432,7 @@ function setSeed(selectedSeed){
 		for(var i = 0; i < seedObject.cols; i++){
 
 			if(seedObject.pattern[j][i] == 1){
-				var cell = getCellByID(_cellPosGrid[startPosRow][0][currentPosCol]);
-				setCellState(cell);
+				setCellState(_cellPosGrid[startPosRow][0][currentPosCol]);
 			}
 			
 			currentPosCol++;
@@ -358,14 +450,14 @@ function getLivingNeighbours(gridIndex){
 	
 	if(!isFirstRow(rowIndex)){
 
-		var cellState = getCellStateByID(_cellPosGrid[rowIndex-1][0][colIndex]);
+		var cellState = _cellPosGrid[rowIndex-1][0][colIndex]._cellState; 
 		if(cellState){
 			
 			neighbourCount++;
 		}
 		
 		if(!isFirstColumn(colIndex)){
-			var cellState = getCellStateByID(_cellPosGrid[rowIndex-1][0][colIndex-1]);
+			var cellState = _cellPosGrid[rowIndex-1][0][colIndex-1]._cellState; 
 			
 			if(cellState){
 				neighbourCount++;
@@ -373,7 +465,7 @@ function getLivingNeighbours(gridIndex){
 		}
 		
 		if(!isLastColumn(colIndex)){
-			var cellState = getCellStateByID(_cellPosGrid[rowIndex-1][0][colIndex+1]);
+			var cellState = _cellPosGrid[rowIndex-1][0][colIndex+1]._cellState; 
 			
 			if(cellState){
 				neighbourCount++;
@@ -382,13 +474,13 @@ function getLivingNeighbours(gridIndex){
 	}
 	
 	if(!isLastRow(rowIndex)){
-		var cellState = getCellStateByID(_cellPosGrid[rowIndex+1][0][colIndex]);
+		var cellState = _cellPosGrid[rowIndex+1][0][colIndex]._cellState; 
 		if(cellState){
 			neighbourCount++;
 		}		
 		
 		if(!isFirstColumn(colIndex)){
-			var cellState = getCellStateByID(_cellPosGrid[rowIndex+1][0][colIndex-1]);
+			var cellState = _cellPosGrid[rowIndex+1][0][colIndex-1]._cellState;
 			
 			if(cellState){
 				neighbourCount++;
@@ -396,7 +488,7 @@ function getLivingNeighbours(gridIndex){
 		}
 		
 		if(!isLastColumn(colIndex)){
-			var cellState = getCellStateByID(_cellPosGrid[rowIndex+1][0][colIndex+1]);
+			var cellState = _cellPosGrid[rowIndex+1][0][colIndex+1]._cellState; 
 			
 			if(cellState){
 				neighbourCount++;
@@ -406,7 +498,7 @@ function getLivingNeighbours(gridIndex){
 	
 	
 	if(!isFirstColumn(colIndex)){
-		var cellState = getCellStateByID(_cellPosGrid[rowIndex][0][colIndex-1]);
+		var cellState = _cellPosGrid[rowIndex][0][colIndex-1]._cellState; 
 		
 		if(cellState){
 			neighbourCount++;
@@ -414,7 +506,7 @@ function getLivingNeighbours(gridIndex){
 	}
 	
 	if(!isLastColumn(colIndex)){
-		var cellState = getCellStateByID(_cellPosGrid[rowIndex][0][colIndex+1]);
+		var cellState = _cellPosGrid[rowIndex][0][colIndex+1]._cellState; 
 		
 		if(cellState){
 			neighbourCount++;
@@ -422,27 +514,6 @@ function getLivingNeighbours(gridIndex){
 	}
 	
 	return neighbourCount;
-}
-
-function getCellStateByID(cellID){
-	for(var i = 0; i< _cellGrid.children.length; i++){
-		if(_cellGrid.children[i].id == cellID){
-			return 	_cellGrid.children[i]._cellState;
-		}	
-	}
-	
-	return false;
-}
-
-
-function getCellByID(cellID){
-	for(var i = 0; i< _cellGrid.children.length; i++){
-		if(_cellGrid.children[i].id == cellID){
-			return 	_cellGrid.children[i];
-		}	
-	}
-	
-	return false;
 }
 
 function isFirstRow(rowIndex){
@@ -474,7 +545,7 @@ function isLastColumn(colIndex){
 }
 
 function resetCellStates(){
-	for(var i = 0; i < _cellGrid.children.length; i++){
+	for(var i = 0, len=_cellGrid.children.length; i<len;++i){
 		forceKillCell(_cellGrid.children[i]);
 	}	
 }
